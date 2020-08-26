dc90f2024a0948fe97490eda16c7b0a64282dbba
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-08-26 18:11:54 +0300

    add controller and views for Cashiers.cs

---
+++ CoffeeHouse/Controllers/CashiersController.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoffeeHouse.Data;
+using CoffeeHouse.Models;
+
+namespace CoffeeHouse.Controllers
+{
+    public class CashiersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CashiersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cashiers.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserName,FullName")] Cashier cashier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cashier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cashier);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cashier = await _context.Cashiers
+                .AsNoTracking()
+                .SingleOrDefaultAsync(c => c.Id == id);
+            if (cashier == null)
+            {
+                return NotFound();
+            }
+            return View(cashier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserName,FullName")] Cashier cashier)
+        {
+            if (id != cashier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cashier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CashierExists(cashier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cashier);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cashier = await _context.Cashiers
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (cashier == null)
+            {
+                return NotFound();
+            }
+
+            return View(cashier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cashier = await _context.Cashiers
+                .AsNoTracking()
+                .SingleAsync(c => c.Id == id);
+            _context.Cashiers.Remove(cashier);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CashierExists(int id)
+        {
+            return _context.Cashiers
+                .AsNoTracking()
+                .Any(e => e.Id == id);
+        }
+    }
+}
