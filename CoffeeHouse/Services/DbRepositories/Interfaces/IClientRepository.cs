d574aa5f9b658b130b9d2d124ef3e84b210853ab
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-08-27 16:50:04 +0300

    add methods to IClientRepository.cs

--- CoffeeHouse/Services/DbRepositories/Interfaces/IClientRepository.cs
+++ CoffeeHouse/Services/DbRepositories/Interfaces/IClientRepository.cs
@@ -1,8 +1,13 @@
 ﻿using CoffeeHouse.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoffeeHouse.Services.DbRepositories.Interfaces
 {
     public interface IClientRepository : IRepository<Client>
     {
+        IQueryable<Client> GetAllOrderedByName();
+        Client GetById(int id);
+        Task<Client> GetByIdAsync(int id);
     }
 }
