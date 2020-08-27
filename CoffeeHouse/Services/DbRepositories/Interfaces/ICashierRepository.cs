cd54cbe9522063211f5a4b8c2c5017e6250105ba
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-08-27 16:14:17 +0300

    add methods to ICashierRepository.cs

--- CoffeeHouse/Services/DbRepositories/Interfaces/ICashierRepository.cs
+++ CoffeeHouse/Services/DbRepositories/Interfaces/ICashierRepository.cs
@@ -1,8 +1,13 @@
 ﻿using CoffeeHouse.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoffeeHouse.Services.DbRepositories.Interfaces
 {
     public interface ICashierRepository : IRepository<Cashier>
     {
+        IQueryable<Cashier> GetAllOrderedByFullName();
+        Cashier GetById(int id);
+        Task<Cashier> GetByIdAsync(int id);
     }
 }
