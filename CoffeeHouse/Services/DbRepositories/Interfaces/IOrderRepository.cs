d577f62237869661d5e888475b4f737df09ff34a
Alexey Zhadan <alexeyzhadan.w@gmail.com>
2020-08-27 20:08:02 +0300

    add methods to IOrderRepository.cs

--- CoffeeHouse/Services/DbRepositories/Interfaces/IOrderRepository.cs
+++ CoffeeHouse/Services/DbRepositories/Interfaces/IOrderRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CoffeeHouse.Models;
+using System.Linq;
 
 namespace CoffeeHouse.Services.DbRepositories.Interfaces
 {
     public interface IOrderRepository : IDefaultEntityRepository<Order>
     {
+        IQueryable<Order> GetAllWithCashiersAndClientsOrderByDate();
     }
 }
--- CoffeeHouse/Services/DbRepositories/OrderRepository.cs
+++ CoffeeHouse/Services/DbRepositories/OrderRepository.cs
@@ -1,6 +1,8 @@
 ﻿using CoffeeHouse.Data;
 using CoffeeHouse.Models;
 using CoffeeHouse.Services.DbRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CoffeeHouse.Services.DbRepositories
 {
@@ -9,6 +11,15 @@ namespace CoffeeHouse.Services.DbRepositories
         public OrderRepository(ApplicationDbContext context) 
             : base(context)
         {
+        }
+
+        public IQueryable<Order> GetAllWithCashiersAndClientsOrderByDate()
+        {
+            return _db.Set<Order>()
+                .Include(o => o.Cashier)
+                .Include(o => o.Client)
+                .OrderByDescending(o => o.Date)
+                .AsNoTracking();
         }
     }
 }
